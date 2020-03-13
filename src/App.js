014b066b3a27f61eeadce3f76da4db490b709844
kj2386 <kj2386@gmail.com>
2020-03-12 21:53:43 -0600

    login and signup done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { connect } from 'react-redux';
+import BaseRouter from './routes';
+import * as actions from './store/actions/auth';
+import 'semantic-ui-css/semantic.min.css';
+import Home from './components/Home';
 
+class App extends Component {
+  componentDidMount() {
+    this.props.onTryAutoSignup();
+  }
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  render() {
+    return (
+      <Router>
+        <Home {...this.props}>
+          <BaseRouter />
+        </Home>
+      </Router>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoSignup: () => dispatch(actions.authCheckState())
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import Hoc from './hoc/Hoc';
+
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Home from './components/Home';
+
+const BaseRouter = () => (
+  <Hoc>
+    <Route path="/login" component={Login} />
+    <Route path="/signup" component={Signup} />
+    <Route exact path="/" component={Home} />
+  </Hoc>
+);
+
+export default BaseRouter;
